1206c749b38c632112e27577fe28bc360cd5c96f
Nikolay Mikitenko <Nikolay.Mikitenko@fuib.com>
2021-10-19 12:20:33 +0300

    Added new version of export

--- dags/common/postgres_to_filesystem.py
+++ dags/common/postgres_to_filesystem.py
@@ -1,47 +1,44 @@
 from airflow.operators.python_operator import PythonOperator
-from airflow.operators.postgres_operator import PostgresOperator
+#from airflow.operators.postgres_operator import PostgresOperator
 from airflow.operators.postgres_operator import PostgresHook
 from pathlib import Path
 import csv
 import os
 
 class ExportDataFromPostgresToFileSystem(PythonOperator):
-    #def __init__(self, sql: str, parameters: dict, postgres_conn_id: str, file_system_path: Path, *args, **kwargs):
-    def __init__(self, postgres_conn_id: str, file_system_path: Path, *args, **kwargs):        
+    def __init__(self, postgres_conn_id: str, file_system_path: Path, table: str, *args, **kwargs):
         super(PythonOperator, self).__init__(*args, **kwargs)
         self.postgres_conn_id = postgres_conn_id
         self.file_system_path = file_system_path
+        self.templates_dict={}
+        self.op_args=[]
+        self.op_kwargs={}
+        self.table = table
         
-    def execute(self, context, table: str):
-        #Declare parameters
-        parameters={'table': table}
-
-        #Load script template for export data
-        with open('scripts/get_table_all_data', 'r') as sql_script:
-            sql = sql_script.read()
-        self.log.info('Executing: %s', self.sql)
+    def execute(self, context):
+        sql=f"SELECT * FROM {self.table}"
+        self.log.info('Executing: %s', sql)
 
         #Execute query
         self.hook = PostgresHook(postgres_conn_id=self.postgres_conn_id)
         with self.hook.get_conn() as conn:
             with conn.cursor() as cur:
-                if parameters is not None:
-                    self.log.info("{} with parameters {}".format(sql, parameters))
-                    cur.execute(sql, parameters)
-                else:
-                    self.log.info(sql)
-                    cur.execute(sql)
+
+                cur.execute(sql)
+                columns = [i[0] for i in cur.description]
                 #rows = cur.fetchall()
 
                 #Check if folder for data exists
                 os.makedirs(self.file_system_path, exist_ok=True)
 
-                #Save data to FileSystem
-                file_path = os.path.join(self.file_system_path, table + '.csv')
-                with open(f'/data/{table}.csv', 'w') as f:
+                #Save data to FileSystem                
+                file_path = os.path.join(self.file_system_path, self.table + '.csv')
+                self.log.info(f'Write data from table {self.table} to file {file_path}')
+                with open(file_path, 'w') as f:
                     file = csv.writer(f)
+                    file.writerow(columns)
                     file.writerows(cur.fetchall())
-                self.log.info(f'Write data from table {table} to file {file_path}')
-
+                self.log.info(f'Data from table {self.table} successfully writed to file {file_path}')
+                
         for output in self.hook.conn.notices:
-            self.log.info(output) 
+            self.log.info(output)
--- dags/export_data_from_postgres_to_filesystem.py
+++ dags/export_data_from_postgres_to_filesystem.py
@@ -1,18 +1,16 @@
 from datetime import datetime
 
 from airflow import DAG
-#from airflow.operators.postgres_operator import PostgresOperator
-#from airflow.operators.python_operator import PythonOperator
 from common.postgres_to_filesystem import ExportDataFromPostgresToFileSystem
 import os
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.postgres_operator import PostgresOperator
 
 dag = DAG(
     dag_id='export_data_from_Postgres_to_FileSystem_dag'
     , start_date=datetime(2021, 1, 1)
     , schedule_interval=None
 )
-
 
 tables = ['aisles', 'clients']
 tables_tasks = []
@@ -21,12 +19,14 @@ for table in tables:
     tables_tasks.append(
         ExportDataFromPostgresToFileSystem(
             task_id=f'extract_table_{table}',
-            dag=dag,            
-            postgres_conn_id= 'dsho', 
-            file_system_path= os.path.join('.', 'data', 'dshop_data')
+            dag=dag,
+            postgres_conn_id='dshop', 
+            file_system_path=os.path.join('home', 'user', 'data', 'dshop_data'),
+            #file_system_path=os.path.join('.', 'data', 'dshop_data'),
+            table=table,
         )
     )
 
 start_task = DummyOperator(task_id='start', dag=dag)
-end_task = DummyOperator(task_id='start', dag=dag)
+end_task = DummyOperator(task_id='end', dag=dag)
 start_task >> tables_tasks >> end_task
