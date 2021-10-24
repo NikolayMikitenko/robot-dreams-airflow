aa4efa5353a0ee7e6bb13fc71920a19b9a94f480
Nikolay Mikitenko <Nikolay.Mikitenko@fuib.com>
2021-10-24 11:09:31 +0300

    Added connect to hdfs

---
+++ dags/export_data_from_postgres_to_hdfs.py
@@ -0,0 +1,54 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.postgres_operator import PostgresHook
+from airflow.providers.apache.hdfs.hooks.hdfs import *
+from airflow.operators.python_operator import PythonOperator
+
+dag = DAG(
+    dag_id='export_data_from_Postgres_to_HDFS_dag'
+    , start_date=datetime(2021, 1, 1)
+    , schedule_interval=None
+)
+
+postgres_conn_id='dshop'
+
+def get_tables_list(postgres_conn_id: str):
+    hook = PostgresHook(postgres_conn_id=postgres_conn_id)
+    with hook.get_conn() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE';")
+            return cur.fetchall()
+
+tables = get_tables_list(postgres_conn_id)        
+
+tables_tasks = []
+
+def export_data_dump_to_hdfs():
+    import logging 
+    cl = HDFSHook('local_hdfs').get_conn()
+    logging.info(cl.ls(["/"]))
+
+t1 = PythonOperator(
+    task_id='db_data_to_hdfs',
+    #f'extract_table_{table[0]}',
+    dag=dag,
+    #postgres_conn_id='dshop', 
+    #file_system_path=os.path.join('.', 'data', 'dshop_data'),
+    #table=table[0],
+    python_callable=exoport_data_dump_tp_hdfs    
+)
+
+#for table in tables:
+#    tables_tasks.append(
+#        cl = HDFSHook('local_hdfs').get_conn()
+#        cl.ls(["/"])
+        
+        #ExportDataFromPostgresToFileSystemOperator(
+        #    task_id=f'extract_table_{table[0]}',
+        #    dag=dag,
+        #    postgres_conn_id='dshop', 
+        #    file_system_path=os.path.join('.', 'data', 'dshop_data'),
+        #    table=table[0],
+        #    python_callable=empty
+        #)
+#    )
